b20b7d771c909fb2f068d79ab4125f7a17a1eccc
Jonas Rodrigues <jonasrod@gmail.com>
2016-11-23 15:28:57 -0200

    alteracoes cardapio

--- controllers/cardapio.php
+++ controllers/cardapio.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
 require 'models/categoria_model.php';
+require 'models/complemento_model.php';
 
 class Cardapio extends Controller {
 
@@ -9,7 +10,7 @@ class Cardapio extends Controller {
 		//Auth::handleLogin();
 	}
 
-	/** 
+	/**
 	* Metodo index
 	*/
 	public function index()
@@ -22,13 +23,13 @@ class Cardapio extends Controller {
 		$this->view->render( "footer" );
 	}
 
-	/** 
+	/**
 	* Metodo editForm
 	*/
 	public function form( $id = NULL )
 	{
 		Session::init();
-		
+
 		$this->view->title = "Cadastrar Cardapio";
 		$this->view->action = "create";
 
@@ -40,6 +41,9 @@ class Cardapio extends Controller {
 
 		$categoriaModel = new Categoria_Model();
 		$this->view->listaCategoria = $categoriaModel->listarCategoriaPorTipo('Cardapio');
+
+		$complementoModel = new Complemento_Model();
+		$this->view->listarComplemento = $complementoModel->listarComplemento();
 
 		if( $id == NULL )
 		{
@@ -72,39 +76,39 @@ class Cardapio extends Controller {
 		$this->view->render( "footer" );
 	}
 
-	/** 
+	/**
 	* Metodo create
 	*/
 	public function create()
 	{
 		$data = array(
-			'item' => $_POST["item"], 
-			'descricao' => $_POST["descricao"], 
-			'preco' => Data::formataMoedaBD(str_replace('R$', '', $_POST["preco"])), 
-			'ativo' => isset($_POST["ativo"]) ? $_POST["ativo"] : 0, 
-			'promocao' => isset($_POST["promocao"]) ? $_POST["promocao"] : 0, 
-			'imagem' => $_POST["imagem"], 
-			'id_categoria' => $_POST["id_categoria"], 
+			'item' => $_POST["item"],
+			'descricao' => $_POST["descricao"],
+			'preco' => Data::formataMoedaBD(str_replace('R$', '', $_POST["preco"])),
+			'ativo' => isset($_POST["ativo"]) ? $_POST["ativo"] : 0,
+			'promocao' => isset($_POST["promocao"]) ? $_POST["promocao"] : 0,
+			'imagem' => $_POST["imagem"],
+			'id_categoria' => $_POST["id_categoria"],
 		);
-		
+
 		$this->model->create( $data ) ? $msg = base64_encode( "OPERACAO_SUCESSO" ) : $msg = base64_encode( "OPERACAO_ERRO" );
 
 		header("location: " . URL . "cardapio?st=".$msg);
 	}
 
-	/** 
+	/**
 	* Metodo edit
 	*/
 	public function edit( $id )
 	{
 		$data = array(
-			'item' => $_POST["item"], 
-			'descricao' => $_POST["descricao"], 
-			'preco' => Data::formataMoedaBD($_POST["preco"]), 
-			'ativo' => $_POST["ativo"], 
-			'promocao' => $_POST["promocao"], 
-			'imagem' => $_POST["imagem"], 
-			'id_categoria' => $_POST["id_categoria"], 
+			'item' => $_POST["item"],
+			'descricao' => $_POST["descricao"],
+			'preco' => Data::formataMoedaBD($_POST["preco"]),
+			'ativo' => $_POST["ativo"],
+			'promocao' => $_POST["promocao"],
+			'imagem' => $_POST["imagem"],
+			'id_categoria' => $_POST["id_categoria"],
 		);
 
 		$this->model->edit( $data, $id ) ? $msg = base64_encode( "OPERACAO_SUCESSO" ) : $msg = base64_encode( "OPERACAO_ERRO" );
@@ -112,7 +116,7 @@ class Cardapio extends Controller {
 		header("location: " . URL . "cardapio?st=".$msg);
 	}
 
-	/** 
+	/**
 	* Metodo delete
 	*/
 	public function delete( $id )
@@ -128,58 +132,58 @@ class Cardapio extends Controller {
 	public function wideimage_ajax()
 	{
 		Session::init();
-		
+
 		require_once 'util/wideimage/WideImage.php';
-		
+
 		date_default_timezone_set("Brazil/East");
-		
+
 		$name 	= $_FILES['files']['name'];
 		$tmp_name = $_FILES['files']['tmp_name'];
-		
+
 		$allowedExts = array(".gif", ".jpeg", ".jpg", ".png");
-		
+
 		// Verifica a acao para pegar a variavel do path correta
 		Session::get('act_cardapio') == 'create' ? $var_path = Session::get('path_cardapio') : $var_path = Session::get('path_edit_cardapio');
-		
+
 		$dir = 'public/img/cardapio/'. $var_path .'/';
-		
+
 		for($i = 0; $i < count($tmp_name); $i++)
 		{
 			$ext = strtolower(substr($name[$i],-4));
-			
+
 			if(in_array($ext, $allowedExts))
 			{
 				$new_name = strtolower( PREFIX_SESSION ).date('Ymd_his').'_'.$name[$i];
-				
+
 				// cria a img default =========================================
 				$image = WideImage::load( $tmp_name[$i] );
-				
+
 				$image = $image->resize(800, 600, 'inside');
 				//$image = $image->crop('center', 'center', 170, 180);
-		
+
 				// verifica so o diretorio existe
 				// caso contrario, criamos o diretorio com permissao para escrita
 				if( !is_dir( $dir ) )
 					mkdir( $dir, 0777, true);
-				
+
 				$image->saveToFile( $dir . $new_name );
-				
+
 				// cria a img thumb ==========================================
 				$image_thumb = WideImage::load( $tmp_name[$i] );
 				$image_thumb = $image_thumb->resize(170, 150, 'outside');
 				$image_thumb = $image_thumb->crop('center', 'center', 170, 150);
-				
+
 				$dir_thumb = $dir.'thumb/';
 				// verifica so o diretorio existe
 				// caso contrario, criamos o diretorio com permissao para escrita
 				if( !is_dir( $dir_thumb ) )
 					mkdir( $dir_thumb, 0777, true);
-				
+
 				$image_thumb->saveToFile( $dir_thumb . $new_name );
 			}
 		}
-		
+
 		echo json_encode($new_name);
-		
+
 	}
 }
--- views/cardapio/form.php
+++ views/cardapio/form.php
@@ -1,6 +1,7 @@
 <!-- Styles -->
 <link href="<?php echo URL?>util/jqueryfiler/css/jquery.filer.css" rel="stylesheet">
 <link href="<?php echo URL?>util/jqueryfiler/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet">
+<link href="<?php echo URL?>public/js/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
 
 <!-- Jvascript -->
 <script src="<?php echo URL?>util/jqueryfiler/js/jquery.filer.min.js" type="text/javascript"></script>
@@ -23,69 +24,94 @@
 
 
 <div class="row">
+<div id="wizard" class="form_wizard wizard_horizontal">
+<ul class="wizard_steps">
+  <li>
+	<a href="#step-1">
+	  <span class="step_no">1</span>
+	  <span class="step_descr">Detalhes do Cardápio</span>
+	</a>
+  </li>
+  <li>
+	<a href="#step-2">
+	  <span class="step_no">2</span>
+	  <span class="step_descr">Complementos</span>
+	</a>
+  </li>
+  <li>
+	<a href="#step-3">
+	  <span class="step_no">3</span>
+	  <span class="step_descr">Disponibilidade</span>
+	</a>
+  </li>
+</ul>
+
+<div id="step-1">
 
 <div class="col-md-6 col-sm-6 col-lg-6 col-xs-12">
-<form id="form1" name="form1" method="post" action="<?php echo URL;?>cardapio/<?php echo $this->action;?>/" class="form-horizontal">
+
+<form id="form-item" name="form1" method="post" action="<?php echo URL;?>cardapio/<?php echo $this->action;?>/"
+	data-parsley-validate class="form-horizontal">
 <input type="hidden" name="idCardapio" value="<?=$this->obj->getId_cardapio()?>" />
 
 <div class="form-group">
-	<label for="item" class="col-md-2 col-sm-2 col-xs-12 control-label">Item</label> 
-	<div class="col-md-9 col-sm-9 col-xs-12"> 
+	<label for="item" class="col-md-2 col-sm-2 col-xs-12 control-label">Item</label>
+	<div class="col-md-9 col-sm-9 col-xs-12">
 		<input type="text" name="item" id="item"  class="form-control col-md-7 col-xs-12" required="required" value="<?=$this->obj->getItem()?>" />
 	</div>
 </div>
 
 <div class="form-group">
-	<label for="descricao" class="col-md-2 col-sm-2 col-xs-12 control-label">Descricao</label> 
-	<div class="col-md-9 col-sm-9 col-xs-12"> 
+	<label for="descricao" class="col-md-2 col-sm-2 col-xs-12 control-label">Descrição</label>
+	<div class="col-md-9 col-sm-9 col-xs-12">
 		<input type="text" name="descricao" id="descricao"  class="form-control col-md-7 col-xs-12" required="required" value="<?=$this->obj->getDescricao()?>" />
 	</div>
 </div>
 
 <div class="form-group">
-	<label for="preco" class="col-md-2 col-sm-2 col-xs-12 control-label">Preco</label> 
-	<div class="col-md-9 col-sm-9 col-xs-12"> 
+	<label for="preco" class="col-md-2 col-sm-2 col-xs-12 control-label">Preço</label>
+	<div class="col-md-9 col-sm-9 col-xs-12">
 		<input type="text" name="preco" id="preco"  class="form-control col-md-7 col-xs-12" required="required" value="<?=$this->obj->getPreco()?>" />
 	</div>
 </div>
 
 <div class="form-group">
-	<label for="ativo" class="col-md-2 col-sm-2 col-xs-12 control-label">Ativo</label> 
+	<label for="ativo" class="col-md-2 col-sm-2 col-xs-12 control-label">Ativo</label>
 	<div class="col-md-9 col-sm-9 col-xs-12">
 		<div class="checkbox">
           <label class="">
             <div class="icheckbox_flat-green checked" style="position: relative;">
-            <input type="checkbox" name="ativo" id="ativo" class="flat" <?=$this->obj->getAtivo() == 1 ? 'checked="checked"': ''?> 
-            	style="position: absolute; opacity: 0;" value="1">
-            <ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
-          </label>
-        </div>
+				<input type="checkbox" name="ativo" id="ativo" class="flat" <?=$this->obj->getAtivo() == 1 ? 'checked="checked"': ''?>
+				style="position: absolute; opacity: 0;" value="1">
+				<ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
+			</label>
+		</div>
 	</div>
 </div>
 
 <div class="form-group">
-	<label for="promocao" class="col-md-2 col-sm-2 col-xs-12 control-label">Promocao</label> 
+	<label for="promocao" class="col-md-2 col-sm-2 col-xs-12 control-label">Promocao</label>
 	<div class="col-md-9 col-sm-9 col-xs-12">
 		<div class="checkbox">
-          <label class="">
-            <div class="icheckbox_flat-green checked" style="position: relative;">
-            <input type="checkbox" name="promocao" id="promocao" class="flat" <?=$this->obj->getPromocao() == 1 ? 'checked="checked"': ''?> style="position: absolute; opacity: 0;" value="1">
-            <ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
-          </label>
-        </div>
-	</div>
+			<label class="">
+				<div class="icheckbox_flat-green checked" style="position: relative;">
+					<input type="checkbox" name="promocao" id="promocao" class="flat" <?=$this->obj->getPromocao() == 1 ? 'checked="checked"': ''?> style="position: absolute; opacity: 0;" value="1">
+					<ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins></div>
+				</label>
+			</div>
+		</div>
 </div>
-
+<!--
 <div class="form-group">
-	<label for="imagem" class="col-md-2 col-sm-2 col-xs-12 control-label">Imagem</label> 
-	<div class="col-md-9 col-sm-9 col-xs-12"> 
+	<label for="imagem" class="col-md-2 col-sm-2 col-xs-12 control-label">Imagem</label>
+	<div class="col-md-9 col-sm-9 col-xs-12">
 		<input type="text" name="imagem" id="imagem"  class="form-control col-md-7 col-xs-12" value="<?=$this->obj->getImagem()?>" />
 	</div>
 </div>
-
+-->
 <div class="form-group">
-	<label for="id_categoria" class="col-md-2 col-sm-2 col-xs-12 control-label">Categoria</label> 
-	<div class="col-md-9 col-sm-9 col-xs-12"> 
+	<label for="id_categoria" class="col-md-2 col-sm-2 col-xs-12 control-label">Categoria</label>
+	<div class="col-md-9 col-sm-9 col-xs-12">
 	<select name="id_categoria" id="id_categoria"  class="form-control col-md-7 col-xs-12" required="required">
 		<option value=""></option>
 		<?php
@@ -101,30 +127,30 @@
 	</select>
 	</div>
 </div>
-
+<!--
 <div class="form-group">
 	<div class="col-sm-10  col-sm-offset-2">
 		<input type="submit" name="salvar" id="salvar" value="Salvar" class="btn btn-success" />
 		<a href="<?php echo URL; ?>cardapio" class="btn btn-primary">Cancelar</a>
 	</div>
 </div>
-
-</form>
+-->
 </div>
+</form>
 
 <div class="col-md-6 col-sm-6 col-lg-6 col-xs-12">
-				
+
 	<!-- debug <form name="form5" action="http://localhost/picolan/cardapio/wideimage_ajax/" method="post" enctype="multipart/form-data" >
 		<input type="file" name="files[]"  multiple="multiple" >
 		<input type="submit">
 	</form> -->
-	 
+
 	<input type="hidden" name="action_post" id="action_post" value="<?php echo $this->method_upload; ?>">
 	<input type="file" name="files[]" id="filer_input2" multiple="multiple">
 
 	<div id="output-files">
 	<ul class="jFiler-items-list jFiler-items-grid" style="padding: 0">
-	
+
 		<?php if( $this->path != '' ) { ?>
 		<?php foreach ( Data::getImgPost( 'cardapio/' . $this->path, true ) as $img ) { ?>
 		<li class="jFiler-item">
@@ -132,12 +158,19 @@
 				<div class="jFiler-item-inner">
 					<div class="jFiler-item-thumb"><img alt="" src="<?=URL.$img?>" ></div>
 					<div class="jFiler-item-assets jFiler-row">
-						
+
 						<ul class="list-inline pull-right">
 							<?php $link_img = str_replace('/thumb/', '/', $img);?>
 							<li>
-								<button class="bt-copy btn btn-info btn-xs" data-clipboard-action="copy" data-clipboard-text="<?='../../'.$link_img?>"><i class="glyphicon glyphicon-link"></i></button>
-								<a href="<?php echo URL?>cardapio/delete_img/<?php echo base64_encode($img);?>"  class="btn btn-danger btn-xs"><i class="icon-jfi-trash jFiler-item-trash-action"></i></a>
+								<button class="bt-copy btn btn-info btn-xs"
+									data-clipboard-action="copy"
+									data-clipboard-text="<?='../../'.$link_img?>">
+									<i class="glyphicon glyphicon-link"></i>
+								</button>
+								<a href="<?php echo URL?>cardapio/delete_img/<?php echo base64_encode($img);?>"
+									class="btn btn-danger btn-xs">
+									<i class="icon-jfi-trash jFiler-item-trash-action"></i>
+								</a>
 							</li>
 						</ul>
 					</div>
@@ -150,19 +183,201 @@
 	</div>
 
 </div><!-- col-md-4 -->
+</div><!-- step-1 -->
+<div id="step-2">
+	<div class="row">
+		<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
+		  <div class="modal-dialog modal-lg">
+			<div class="modal-content">
 
-</div>
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+				</button>
+				<h4 class="modal-title" id="myModalLabel">Complementos</h4>
+			  </div>
+			  <div class="modal-body">
+				  <table id="datatable-responsive" class="table table-striped" cellspacing="0" width="100%">
+					<thead>
+					<tr>
+						<th>Complemento </th>
+						<th>Descrição </th>
+						<th>Preço </th>
+						<th>Categoria </th>
+						<th></th>
+					</tr>
+					</thead>
+					<tbody>
+					<?php foreach( $this->listarComplemento as $complemento ) { ?>
+					<tr>
+						<td><?php echo $complemento->getComplemento(); ?></td>
+						<td><?php echo $complemento->getDescricao(); ?></td>
+						<td><?php echo Data::formataMoeda($complemento->getPreco()); ?></td>
+						<td><?php echo $complemento->getCategoria()->getCategoria(); ?></td>
+						<td align="right">
+							<a  class="btn btn-dark btn-sm" style="cursor: pointer;"
+								onclick="addDataSet([
+									<?php echo $complemento->getId_complemento(); ?>,
+									'<?php echo $complemento->getComplemento(); ?>',
+									'<?php echo $complemento->getDescricao(); ?>',
+									'<?php echo Data::formataMoeda($complemento->getPreco()); ?>',
+									'<?php echo $complemento->getCategoria()->getCategoria(); ?>',
+									'Remover'
+								])">
+								<i class="glyphicon glyphicon-plus"></i>
+							</a>
+						</td>
+						</tr>
+					<?php } ?>
+					</tbody>
+				</table>
+			  </div>
+			  <div class="modal-footer">
+				  <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+			  </div>
+
+			</div>
+		  </div>
+		</div>
+	</div>
+	<!-- Large modal -->
+	<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">
+		<i class="fa fa-plus"></i>
+		Adicionar
+	</button>
+	<br />
+	<table id="datatable-complemento" class="table table-striped" cellspacing="0" width="100%">
+		<thead>
+		<tr>
+			<th>Cod </th>
+			<th>Complemento </th>
+			<th>Descrição </th>
+			<th>Preço </th>
+			<th>Categoria </th>
+			<th></th>
+		</tr>
+		</thead>
+		<tbody></tbody>
+	</table>
+</div><!-- step-2 -->
+<div id="step-3">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h4>Dias em que o item estará disponível</h4>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<input type="checkbox" name="segunda" value="segunda" checked  />Segunda
+			&nbsp;&nbsp;
+			<input type="checkbox" name="terca" value="segunda" checked />Terça
+			&nbsp;&nbsp;
+			<input type="checkbox" name="quarta" value="segunda" checked />Quarta
+			&nbsp;&nbsp;
+			<input type="checkbox" name="quinta" value="segunda" checked />Quinta
+			&nbsp;&nbsp;
+			<input type="checkbox" name="sexta" value="segunda" checked />Sexta
+			&nbsp;&nbsp;
+			<input type="checkbox" name="sabado" value="segunda" checked />Sábado
+			&nbsp;&nbsp;
+			<input type="checkbox" name="domingo" value="segunda" checked />Domingo
+			&nbsp;&nbsp;
+		</div>
+	</div>
+</div><!-- step-3 -->
+</div><!-- form wizard -->
+</div><!-- row -->
 
 
 </div>
 </div>
 </div>
 <!-- /.row -->
+<!-- bootstrap progress js -->
+<script src="<?php echo URL?>public/js/progressbar/bootstrap-progressbar.min.js"></script>
+<script src="<?php echo URL?>public/js/nicescroll/jquery.nicescroll.min.js"></script>
+<!-- icheck -->
+<script src="<?php echo URL?>public/js/icheck/icheck.min.js"></script>
+
+<script src="<?php echo URL?>public/js/custom.js"></script>
+<!-- form wizard -->
+<script type="text/javascript" src="<?php echo URL?>public/js/wizard/jquery.smartWizard.js"></script>
+<!-- pace -->
+<script src="<?php echo URL?>public/js/pace/pace.min.js"></script>
 <script src="<?php echo URL?>public/js/input_mask/jquery.inputmask.js"></script>
 <script src="<?php echo URL?>public/js/input_mask/jquery.maskMoney.min.js"></script>
-<script>
-  var clipboard = new Clipboard('.bt-copy');
-  $(function() {
-    $('#preco').maskMoney({prefix:'R$ ', thousands:'.', decimal:','});
-  })
+<!-- form validation -->
+<script src="<?php echo URL?>public/js/parsley/parsley.min.js"></script>
+<!-- DataTable-->
+<script src="<?php echo URL?>public/js/datatables/jquery.dataTables.min.js"></script>
+<script type="text/javascript">
+	var table;
+
+	function addDataSet(obj) {
+		table.row.add(obj).draw();
+	}
+
+	$('#datatable-complemento tbody').on( 'click', 'button', function () {
+		table
+			.row( $(this).parents('tr') )
+			.remove()
+			.draw();
+	} );
+
+	$(document).ready(function() {
+		// Smart Wizard
+		$('#wizard').smartWizard({
+			labelNext: 'Próximo',
+			labelPrevious: 'Anterior',
+			labelFinish: 'Finalizar',
+			onFinish:onFinishCallback,
+			onLeaveStep: onLeaveStepCallback
+
+		});
+		function onLeaveStepCallback(obj, context) {
+			if (context.fromStep == 1) {
+				var validate = $('#form-item').parsley().validate();
+				console.log("Forma valido: " + validate);
+		  	  	validateFront();
+				return validate;
+			} else {
+				return true;
+			}
+		}
+
+		function onFinishCallback() {
+		  alert('Finish Clicked');
+		}
+
+		$.listen('parsley:field:validate', function() {
+		  validateFront();
+		});
+
+		var validateFront = function() {
+		  if (true === $('#form-item').parsley().isValid()) {
+			$('.bs-callout-info').removeClass('hidden');
+			$('.bs-callout-warning').addClass('hidden');
+		  } else {
+			$('.bs-callout-info').addClass('hidden');
+			$('.bs-callout-warning').removeClass('hidden');
+		  }
+		};
+
+		table = $('#datatable-complemento').DataTable({
+			  "columnDefs": [ {
+			    "targets": 5,
+			    "data": function ( row, type, val, meta ) {
+			      if (type === 'display' || type == 'sorting' || type ==  'filtering') {
+					  row[5] = '<button id="btnExcluir" class="btn btn-danger btn-xs">Remover</button>'
+			    	  return row[5];
+			      }
+			      return row;
+			    }
+			  } ]
+			} );
+	  });
+
+	  var clipboard = new Clipboard('.bt-copy');
+	  $(function() {
+	    $('#preco').maskMoney({prefix:'R$ ', thousands:'.', decimal:','});
+	  })
 </script>
